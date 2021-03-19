ee1606ebb38ad32bcaff9b0ead48ea37b64b2a8e
Thiago Henrique da Silva <thsilva@gmail.com>
2021-03-19 18:51:05 -0300

    connectin manager redis

--- src/Middleware/Drapo/Pipe/DrapoConnectionManagerRedis.cs
+++ src/Middleware/Drapo/Pipe/DrapoConnectionManagerRedis.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ServiceStack.Redis;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,30 +7,58 @@ namespace Sysphera.Middleware.Drapo.Pipe
 {
     public class DrapoConnectionManagerRedis : IDrapoConnectionManager
     {
-        private readonly DrapoMiddlewareOptions _options;
+        private readonly RedisManagerPool _redisManagerPool = null;
         public DrapoConnectionManagerRedis(DrapoMiddlewareOptions options) {
-            _options = options;
+            _redisManagerPool = new RedisManagerPool(options.BackplaneRedis);
+        }
+
+        private string CreateDomainKey(string domain) {
+            return ($"drapo_connection_{domain}");
+        }
+        private string CreateConnectionKey(string domain, string connectionId)
+        {
+            return ($"drapo_connection_{domain}_{connectionId}");
         }
 
         public void Create(string domain, string connectionId)
         {
+            using (IRedisClient client = _redisManagerPool.GetClient()) 
+            {
+                DrapoConnection connection = new DrapoConnection(connectionId, domain);
+                client.Set<DrapoConnection>(this.CreateConnectionKey(domain, connectionId), connection);
+            }
         }
         public bool Remove(string domain, string connectionId)
         {
-            return (false);
+            using (IRedisClient client = _redisManagerPool.GetClient()) 
+            {
+                return(client.Remove(this.CreateConnectionKey(domain, connectionId)));
+            }
         }
 
         public long Count(string domain)
         {
-            return (0);
+            return (this.GetAll(domain).Count);
         }
         public DrapoConnection Get(string domain, string connectionId)
         {
-            return (null);
+            using (IRedisClient client = _redisManagerPool.GetClient()) {
+                return(client.Get<DrapoConnection>(this.CreateConnectionKey(domain, connectionId)));
+            }
         }
         public List<DrapoConnection> GetAll(string domain)
         {
-            return (null);
+            List<DrapoConnection> connections = new List<DrapoConnection>();
+            using (IRedisClient client = _redisManagerPool.GetClient())
+            {
+                foreach (string key in client.GetKeysByPattern(this.CreateDomainKey(domain) + "*")) 
+                {
+                    DrapoConnection connection = client.Get<DrapoConnection>(key);
+                    if (connection != null)
+                        connections.Add(connection);
+                }
+            }
+            return (connections);
         }
     }
 }
