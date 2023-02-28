65f099bc0e3717bc1a4ce5cb0f229056155a5437
Thiago Henrique da Silva <thsilva@gmail.com>
2023-02-28 10:27:03 -0300

    fix iterator with composite in component

--- src/Test/WebDrapo.Test/ReleaseTest.cs
+++ src/Test/WebDrapo.Test/ReleaseTest.cs
@@ -410,6 +410,11 @@ namespace WebDrapo.Test
             ValidatePage("ControlFlowForComponentMultipleSources");
         }
         [TestCase]
+        public void ControlFlowForComponentMultipleSourcesCompositeTest()
+        {
+            ValidatePage("ControlFlowForComponentMultipleSourcesComposite");
+        }
+        [TestCase]
         public void ControlFlowForComponentMultipleSourcesRangeTest()
         {
             ValidatePage("ControlFlowForComponentMultipleSourcesRange");
