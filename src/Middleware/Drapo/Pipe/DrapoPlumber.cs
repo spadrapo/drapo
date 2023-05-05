9fc60b7ec6f15b728b9623e33be998786c158fac
Thiago Henrique da Silva <thsilva@gmail.com>
2023-05-05 17:00:33 -0300

    allow plumber with a specific connection

--- src/Middleware/Drapo/Pipe/DrapoPlumber.cs
+++ src/Middleware/Drapo/Pipe/DrapoPlumber.cs
@@ -24,16 +24,16 @@ namespace Sysphera.Middleware.Drapo.Pipe
             _connectionManager = connectionManager;
         }
 
-        public async Task<bool> Send(DrapoPipeMessage message, DrapoPipeAudienceType recipient)
+        public async Task<bool> Send(DrapoPipeMessage message, DrapoPipeAudienceType recipient, string connectionId = null)
         {
-            IClientProxy proxy = this.GetRecipients(recipient);
+            IClientProxy proxy = this.GetRecipients(recipient, connectionId);
             if (proxy == null)
                 return (await Task.FromResult<bool>(false));
             await proxy.SendAsync(this._options.Config.PipeActionNotify, message);
             return (await Task.FromResult<bool>(true));
         }
 
-        private IClientProxy GetRecipients(DrapoPipeAudienceType recipient)
+        private IClientProxy GetRecipients(DrapoPipeAudienceType recipient, string recipientConnectionId)
         {
             string connectionId = this._requestReader.GetPipeHeaderConnectionId();
             if (string.IsNullOrEmpty(connectionId))
@@ -42,11 +42,13 @@ namespace Sysphera.Middleware.Drapo.Pipe
             if (connection == null)
                 return (null);
             if (recipient == DrapoPipeAudienceType.Others)
-                return (this._hub.Clients.GroupExcept(connection.Domain, new List<string>(){ connectionId }));
+                return (this._hub.Clients.GroupExcept(connection.Domain, new List<string>() { connectionId }));
             else if (recipient == DrapoPipeAudienceType.Me)
                 return (this._hub.Clients.Client(connectionId));
             else if (recipient == DrapoPipeAudienceType.Everyone)
                 return (this._hub.Clients.Group(connection.Domain));
+            else if (recipient == DrapoPipeAudienceType.Connection)
+                return (this._hub.Clients.Client(recipientConnectionId));
             return (null);
         }
 
@@ -70,7 +72,21 @@ namespace Sysphera.Middleware.Drapo.Pipe
             return (await Task.FromResult<List<DrapoConnection>>(connections));
         }
 
-        public bool CheckConnections() {
+        public async Task<List<DrapoConnection>> GetIdentityConnections(long identity)
+        {
+            List<DrapoConnection> connections = await this.GetConnections();
+            List<DrapoConnection> connectionsIdentity = new List<DrapoConnection>();
+            for (int i = connections.Count - 1; i >= 0; i--)
+            {
+                DrapoConnection connection = connections[i];
+                if (connection.Identity == identity)
+                    connectionsIdentity.Add(connection);
+            }
+            return (connectionsIdentity);
+        }
+
+        public bool CheckConnections()
+        {
             return (_connectionManager.Check());
         }
     }
