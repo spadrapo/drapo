3f45a9c60a6cc0c945e48cd0096d05fa0002ea03
Heber Gonçalves Junior <hjunior@tech6group.com>
2024-05-31 12:49:53 -0300

    Coalesce with mustache.

--- src/Middleware/Drapo/ts/DrapoStorage.ts
+++ src/Middleware/Drapo/ts/DrapoStorage.ts
@@ -2238,7 +2238,7 @@ class DrapoStorage {
         if (objectsAggregations !== null)
             return (objectsAggregations);
         //Functions
-        this.ResolveQueryFunctions(query, objects, objectsInformation);
+        await this.ResolveQueryFunctions(query, sector, objects, objectsInformation);
         //Output Array
         if (query.OutputArray != null) {
             const outputArray: any[] = [];
@@ -2334,6 +2334,10 @@ class DrapoStorage {
                     if (objectInformation[functionParameterName] != null)
                         continue;
                     const functionParameterValues: string[] = this.Application.Parser.ParseQueryProjectionFunctionParameterValue(functionParameterName);
+                    //const functionParameterValue: string = functionParameterValues[0];
+                    //if (this.Application.Parser.IsMustache(functionParameterValue)) {
+
+                    //}
                     const sourceProperty: string[] = functionParameterValues[0].split('_');
                     let source: string;
                     let property: string;
@@ -2449,26 +2453,30 @@ class DrapoStorage {
         return (value);
     }
 
-    private ResolveQueryFunctions(query: DrapoQuery, objects: any[], objectsInformation: any[]): void {
+    private async ResolveQueryFunctions(query: DrapoQuery, sector: string, objects: any[], objectsInformation: any[]): Promise<void> {
         for (let i: number = 0; i < query.Projections.length; i++) {
             const projection: DrapoQueryProjection = query.Projections[i];
             if (projection.FunctionName !== null)
-                this.ResolveQueryFunction(projection.Alias, projection.FunctionName, projection.FunctionParameters, objects, objectsInformation);
+                await this.ResolveQueryFunction(projection.Alias, projection.FunctionName, projection.FunctionParameters, objects, objectsInformation, sector);
         }
     }
 
-    private ResolveQueryFunction(projectionAlias: string, functionName: string, functionParameters: string[], objects: any[], objectsInformation: any[]): void {
+    private async ResolveQueryFunction(projectionAlias: string, functionName: string, functionParameters: string[], objects: any[], objectsInformation: any[], sector: string): Promise<void> {
         if (functionName === 'COALESCE')
-            this.ResolveQueryFunctionCoalesce(projectionAlias, functionParameters, objects, objectsInformation);
+            await this.ResolveQueryFunctionCoalesce(projectionAlias, functionParameters, objects, objectsInformation, sector);
     }
 
-    private ResolveQueryFunctionCoalesce(projectionAlias: string, functionParameters: string[], objects: any[], objectsInformation: any[]): void {
+    private async ResolveQueryFunctionCoalesce(projectionAlias: string, functionParameters: string[], objects: any[], objectsInformation: any[], sector: string): Promise<void> {
         for (let i: number = 0; i < objects.length; i++) {
             const object: any = objects[i];
             const objectInformation: any = objectsInformation[i];
             for (let j: number = 0; j < functionParameters.length; j++) {
                 const functionParameter: string = functionParameters[j];
                 const functionParameterName: string = this.ResolveQueryFunctionParameterName(functionParameter);
+                if (this.Application.Parser.IsMustache(functionParameterName)) {
+                    object[projectionAlias] = await this.RetrieveDataValue(sector, functionParameterName);
+                    break;
+                }
                 if (objectInformation[functionParameterName] == null)
                     continue;
                 object[projectionAlias] = objectInformation[functionParameterName];
