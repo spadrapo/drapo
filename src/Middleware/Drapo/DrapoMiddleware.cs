7bc1be721d366e40216ddae2b5d6ee88e499c954
sergio.castelani <scastelani@tech6group.com>
2025-04-10 08:55:02 -0300

    fix about Chrome not displaying the correct source lines in dev tools > Network > Initiator

--- src/Middleware/Drapo/DrapoMiddleware.cs
+++ src/Middleware/Drapo/DrapoMiddleware.cs
@@ -276,7 +276,7 @@ namespace Sysphera.Middleware.Drapo
             }
             string sectionsString = string.Join(',', sections);
             if (string.IsNullOrEmpty(sectionsString)) //return dummy map
-                return @"{""version"":3,""file"":""drapo.js"",""sourceRoot"":"""",""sources"":[""dummy.ts""],""names"":[],""mappings"":"";AAAA,MAAM""}";
+                return @"{""version"":3,""file"":""drapo.js"",""sourceRoot"":"""",""sources"":[""drapo.js""],""names"":[],""mappings"":""""}";
             return $@"{{ ""version"": 3, ""file"": ""drapo.js"", ""sections"": [{sectionsString}] }}";
         }
         #endregion
